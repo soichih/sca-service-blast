1de0a2d143ad4556e366f3b926f7c77bd66b8bb6
Soichi Hayashi <hayashis@iu.edu>
2016-01-04 21:43:18 +0000

    created blastdb.py skelton

--- run.sh
+++ run.sh
@@ -8,26 +8,10 @@ exec 2> >(tee -a ${LOGFILE} >&2)
 env | sort | grep SCA
 cat config.json 
 
-progress_url=$SCA_PROGRESS_URL/${SCA_PROGRESS_KEY}.blastdb
+#progress_url=$SCA_PROGRESS_URL/${SCA_PROGRESS_KEY}.blastdb
+#dbname=pdbaa
 
-#download db
-#dbname=nr
-dbname=pdbaa
-mkdir -p $dbname
-(
-    cd $dbname
-
-    curl -X POST -H "Content-Type: application/json" -d '{"status": "running", "progress": 0, "msg":"Downloading $dbname"}' $progress_url
-    wget -nH --cut-dirs=3 -r ftp://ftp.ncbi.nlm.nih.gov/blast/db/$dbname.*
-
-    if [ -f $dbname.00.tar.gz ];
-    then
-            echo "this is multipart db - untarring .pal or .nal from $dbname.00.tar.gz"
-            tar -vxzf $dbname.00.tar.gz $dbname.*al
-    fi
-
-    curl -X POST -H "Content-Type: application/json" -d '{"status": "finished", "progress": 1, "msg":"Finished downloading $dbname"}' $progress_url
-)
+./blastdb.py
 
 #output products.json
-echo "[{\"type\":\"blastdb\", \"name\":\"$dbname\"}]" > products.json
+#echo "[{\"type\":\"blastdb\", \"name\":\"$dbname\"}]" > products.json
