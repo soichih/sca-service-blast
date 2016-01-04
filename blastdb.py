163cc276b27bc2f2606b47ac6a7391ba1b12c932
Soichi Hayashi <hayashis@iu.edu>
2016-01-04 21:46:11 +0000

    forgot to add some files

---
+++ blastdb.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import json
+import sys
+import os
+
+config_json=open("config.json").read()
+config=json.loads(config_json)
+os.environ["DBNAME"] = config["db"]
+
+if config["source"] == "ncbi":
+    os.system("./download_ncbi.sh")
+
+product = {'type': 'blastdb', 'name': config["db"]}
+with open('products.json', 'w') as out:
+    json.dump([product], out)
---
+++ download_ncbi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#download db
+#DBNAME=nr
+echo "Downloading $DBNAME"
+mkdir -p $DBNAME
+cd $DBNAME
+
+#curl -X POST -H "Content-Type: application/json" -d "{\"status\": \"running\", \"progress\": 0, \"msg\":\"Downloading $DBNAME\"}" $progress_url
+wget -nH --cut-dirs=3 -r ftp://ftp.ncbi.nlm.nih.gov/blast/db/$DBNAME*
+
+if [ -f $DBNAME.00.tar.gz ];
+then
+        echo "this is multipart db - untarring .pal or .nal from $DBNAME.00.tar.gz"
+        tar -vxzf $DBNAME.00.tar.gz $DBNAME.*al
+fi
+
+#curl -X POST -H "Content-Type: application/json" -d "{\"status\": \"finished\", \"progress\": 1, \"msg\":\"Finished downloading $DBNAME\"}" $progress_url
