8bfa6bdf87a9fe19a0f890289f4e8f0f11f9422d
Soichi Hayashi <hayashis@iu.edu>
2016-01-04 21:50:00 +0000

    added run.sh to wrap stdout

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## These three lines will cause stdout/err to go a logfile as well
+LOGFILE=run.log
+exec > >(tee -a ${LOGFILE})
+exec 2> >(tee -a ${LOGFILE} >&2)
+env | sort | grep SCA #debug..
+
+./blastdb.py
